2aa857a674a778727de275c8c6027a7d1aeae4fb
kojin <kojin8mf@me.com>
2025-02-13 14:21:49 +0900

    redeploy 20250213

--- bvbss_site/settings.py
+++ bvbss_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8zkt7z5u$-0%c9tt37^1#xl^6539&n*r7l)!%0-i618v2mg+xx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False # True #☆
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'pythonanywhere.com', 'kojinabc.pythonanywhere.com'] #☆
 
 
 # Application definition
@@ -127,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_COOKIE_SECURE = True #☆
+SESSION_COOKIE_SECURE = True #☆
+
+from django.core.management.utils import get_random_secret_key #☆
+SECRET_KEY = get_random_secret_key()
